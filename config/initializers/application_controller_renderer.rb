5be9ebabb896d5cdb8226148752d0f1dc6d862bc
EC2 Default User <ec2-user@ip-172-31-41-212.ec2.internal>
2021-07-19 11:11:14 +0000

    [Finish]

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f998bb97a7186934715e706d8f8c8a8f4b415c9eaa871b9b512a192fef36ebc008559686f809af7edc3c6c026c342b4716200ab91e4556a4d2ffd4bc13a1c26d'
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  # 以下の行のonly: []内にeditを追加
+    resources :users, only: [:show, :edit, :update]
+  end
